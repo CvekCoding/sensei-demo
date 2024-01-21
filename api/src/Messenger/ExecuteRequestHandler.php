4cddc8cb9e5f506be15a13d2b8fb1de2c28ecfad
Sergey Logachev <serg@baitek.org>
2024-01-21 13:30:53 +0300

    Add linter to expression

--- api/src/Messenger/ExecuteRequestHandler.php
+++ api/src/Messenger/ExecuteRequestHandler.php
@@ -12,22 +12,31 @@ declare(strict_types=1);
 
 namespace App\Messenger;
 
+use ApiPlatform\Validator\Exception\ValidationException;
 use App\Entity\ExecuteRequest;
 use App\ExpressionLanguage\SenseiExpressionProvider;
 use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+use Symfony\Component\ExpressionLanguage\SyntaxError;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 final readonly class ExecuteRequestHandler
 {
+     private ExpressionLanguage $expressionLanguage;
+
     public function __construct(private SenseiExpressionProvider $senseiExpressionProvider)
-    {}
+    {
+        $this->expressionLanguage = new ExpressionLanguage(providers: [$this->senseiExpressionProvider]);
+    }
 
     public function __invoke(ExecuteRequest $request): ExecuteRequest
     {
-        $expressionLanguage = new ExpressionLanguage();
-        $expressionLanguage->registerProvider($this->senseiExpressionProvider);
+        try {
+            $this->expressionLanguage->lint($request->getCommand(), null);
+        } catch (SyntaxError $e) {
+            throw new ValidationException($e->getMessage());
+        }
 
-        return $request->setResult($expressionLanguage->evaluate($request->getCommand(), ['request' => $request]) ?? null);
+        return $request->setResult($this->expressionLanguage->evaluate($request->getCommand(), ['request' => $request]) ?? null);
     }
 }
