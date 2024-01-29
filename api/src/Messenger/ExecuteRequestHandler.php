68a3b52027147875b72796777584afbb834a3c24
Sergey Logachev <serg@baitek.org>
2024-01-29 18:02:19 +0300

    Clean

--- api/src/Messenger/ExecuteRequestHandler.php
+++ api/src/Messenger/ExecuteRequestHandler.php
@@ -38,7 +38,6 @@ final readonly class ExecuteRequestHandler
             throw new ValidationException(message: $e->getMessage(), previous: $e);
         }
 
-//        $request->setCompiled($this->expressionLanguage->compile($request->getCommand()));
         $request->setResult($this->expressionLanguage->evaluate($request->getCommand(), ['request' => $request]) ?? null);
 
         $this->em->flush();
